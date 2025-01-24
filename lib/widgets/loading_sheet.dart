d98df1b288be4c386ca9f93bffe4ab487cecc3c5
AxelYoung <axelyoung@stu.scu.edu.cn>
2025-01-24 12:58:09 +0800

    更新 loading_sheet 文件

---
+++ lib/widgets/loading_sheet.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LoadingSheet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      initialChildSize: 0.1,
+      minChildSize: 0.1,
+      maxChildSize: 1.0,
+      builder: (BuildContext context, ScrollController scrollController) {
+        return Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+          ),
+          child: ListView(
+            controller: scrollController,
+            children: <Widget>[
+              Center(
+                child: CircularProgressIndicator(),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Full Dialog'),
+                        content: Text('This is a full dialog triggered by dragging.'),
+                        actions: <Widget>[
+                          TextButton(
+                            child: Text('Close'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: Text('Show Full Dialog'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
